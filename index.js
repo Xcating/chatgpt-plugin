560624d58559f4cc5f675e2352a2328001f8affa
Xcating <927168329@qq.com>
2023-07-10 15:20:30 +0800

    feat. 更改一些有趣的加载提示

--- index.js
+++ index.js
@@ -32,17 +32,17 @@ for (let i in files) {
 // 启动服务器
 await createServer()
 logger.info(logger.red("-------------\ \ \ ٩(๑˃̵ᴗ˂̵)و / / /-------------"))
-logger.info(logger.cyan(" ________  ___  ___  ________  _________  ________  ________  _________"));
-logger.info(logger.cyan("|\\   ____\\|\\  \\|\\  \\|\\   __  \\|\\___   ___\\\\   ____\\|\\   __  \\|\\___   ___\\"));
-logger.info(logger.cyan("\\ \\  \\___|\\ \\  \\\\\\  \\ \\  \\|\\  \\|___ \\  \\_\\ \\  \\___|\\ \\  \\|\\  \\|___ \\  \\_|"));
-logger.info(logger.cyan(" \\ \\  \\    \\ \\   __  \\ \\   __  \\   \\ \\  \\ \\ \\  \\  __\\ \\   ____\\   \\ \\  \\"));
-logger.info(logger.cyan("  \\ \\  \\____\\ \\  \\ \\  \\ \\  \\ \\  \\   \\ \\  \\ \\ \\  \\|\\  \\ \\  \\___|    \\ \\  \\"));
-logger.info(logger.cyan("   \\ \\_______\\ \\__\\ \\__\\ \\__\\ \\__\\   \\ \\__\\ \\ \\_______\\ \\__\\        \\ \\__\\"));
-logger.info(logger.cyan("    \\|_______|\\|__|\\|__|\\|__|\\|__|    \\|__|  \\|_______|\\|__|         \\|__|"));
-logger.info('ChatGpt-Plugin加载成功')
-logger.info(`当前版本${Config.version}`)
-logger.info('仓库地址 https://github.com/ikechan8370/chatgpt-plugin')
-logger.info('插件群号 559567232')
+logger.info(logger.cyan.bold(" ________  ___  ___  ________  _________  ________  ________  _________"));
+logger.info(logger.cyan.bold("|\\   ____\\|\\  \\|\\  \\|\\   __  \\|\\___   ___\\\\   ____\\|\\   __  \\|\\___   ___\\"));
+logger.info(logger.cyan.bold("\\ \\  \\___|\\ \\  \\\\\\  \\ \\  \\|\\  \\|___ \\  \\_\\ \\  \\___|\\ \\  \\|\\  \\|___ \\  \\_|"));
+logger.info(logger.cyan.bold(" \\ \\  \\    \\ \\   __  \\ \\   __  \\   \\ \\  \\ \\ \\  \\  __\\ \\   ____\\   \\ \\  \\"));
+logger.info(logger.cyan.bold("  \\ \\  \\____\\ \\  \\ \\  \\ \\  \\ \\  \\   \\ \\  \\ \\ \\  \\|\\  \\ \\  \\___|    \\ \\  \\"));
+logger.info(logger.cyan.bold("   \\ \\_______\\ \\__\\ \\__\\ \\__\\ \\__\\   \\ \\__\\ \\ \\_______\\ \\__\\        \\ \\__\\"));
+logger.info(logger.cyan.bold("    \\|_______|\\|__|\\|__|\\|__|\\|__|    \\|__|  \\|_______|\\|__|         \\|__|"));
+logger.info(`${chalk.red.bold('ChatGPT')}${chalk.yellow.bold('-')}${chalk.blue.bold('Plugin')}${chalk.cyan.bold('加载')}${chalk.green.bold('成功!')}`)
+logger.info(`当前插件版本 ${chalk.green.bold(Config.version)}`)
+logger.info(chalk.yellow.bold('仓库地址 https://github.com/ikechan8370/chatgpt-plugin'))
+logger.info(chalk.cyan.bold('ChatGPT-Plugin交流群号 559567232'))
 logger.info(logger.red("-------------\ \ \ ٩(๑˃̵ᴗ˂̵)و / / /-------------"))
 
 export { apps }
