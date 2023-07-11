51c42cfb73a00edd797c2f7f95727d038f0d58d0
Xcating <927168329@qq.com>
2023-07-11 11:48:38 +0800

    fix. 修正读取模型时的错误

--- apps/management.js
+++ apps/management.js
@@ -297,7 +297,7 @@ export class ChatgptManagement extends plugin {
       const models = data.data; // 获取所有的models
       const totalModels = models.length; // 模型总数
       logger.info(logger.cyan('[ChatGPT-plugin]'), logger.yellow(`[配置]`), logger.red(`[查看模型]`), `API可用模型总数:  + ${totalModels}\n读取模型的API为[ ${protectiveUrl} ]`)
-      messages.push(`API可用模型总数:  + ${totalModels}\n读取模型的API为[ ${protectiveUrl} ]`)
+      messages.push(`API可用模型总数: ${totalModels}\n读取模型的API为[ ${protectiveUrl} ]`)
       // 遍历每个model并存储到messages数组中
       models.forEach((model, index) => {
         const message = `${index + 1}. 模型名称: ${model.id}\n所有者: ${model.owned_by}`
