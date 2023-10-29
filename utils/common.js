18156770539a6be73bcd4d4d65d426e6011b14c7
Xcating <Xcating>
2023-10-29 18:52:51 +0800

    fix

--- utils/common.js
+++ utils/common.js
@@ -874,13 +874,13 @@ export function getMaxModelTokens(model = "gpt-3.5-turbo") {
 }
 
 export function getUin (e) {
-  if (getUin(e)) return getUin(e)
+  if (e?.bot?.uin) return e.bot.uin
   if (Array.isArray(Bot.uin)) {
     if (Config.trssBotUin && Bot.uin.indexOf(Config.trssBotUin) > -1) {return Config.trssBotUin}
     else {
         Bot.uin.forEach((u) => {
           if (Bot[u].self_id) {
-            return Bot[u].self_id 
+            return Bot[u].self_id
           }
         })
         return Bot.uin[Bot.uin.length - 1]
